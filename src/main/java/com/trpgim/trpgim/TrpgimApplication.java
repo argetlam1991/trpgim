7ff5cfe9c9455a553c68490069c941775cd86725
Gu Han <guhan@Gus-MacBook-Pro.local>
2018-09-24 22:09:41 -0700

    first commit

---
+++ src/main/java/com/trpgim/trpgim/TrpgimApplication.java
@@ -0,0 +1,12 @@
+package com.trpgim.trpgim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrpgimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrpgimApplication.class, args);
+	}
+}
